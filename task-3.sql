04d93ba06eb789ee44ad936e507b03740cce971f
yoginiparate07 <yoginiparate76@gmail.com>
2024-07-05 10:09:05 +0530

    task3 completed

---
+++ task-3.sql
@@ -0,0 +1,35 @@
+select * from enrollment
+
+select student_name from enrollment
+
+select enrollment_id,student_id,age from enrollment
+
+select semester from enrollment
+
+select * from enrollment
+
+select distinct student_id from enrollment
+
+select distinct course_id from enrollment 
+
+select distinct student_name from enrollment 
+
+select * from enrollment where age < 20
+
+select * from enrollment where age > 22
+
+select * from enrollment where student_name = 'shreya'
+
+select * from enrollment where semester = 'sixthsemester'
+
+select * from enrollment where semester = 'fourthsemester'
+
+select * from enrollment where semester = 'secondsemester' or semester = 'thirdsemester'
+	
+select * from enrollment where age > 20 or age < 22
+
+select * from enrollment where fees > 35000 or fees < 25000
+
+
+
+
